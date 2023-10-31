cf173c9c6db7212f807ce01f69f4348dded05b76
fazekas28 <148578127+fazekas28@users.noreply.github.com>
2023-10-31 11:40:52 -0300

    Update axios.ts

--- esg_novo/src/axios.ts
+++ esg_novo/src/axios.ts
@@ -2,5 +2,5 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-    baseURL: 'https://gifted-bass-shoulder-pads.cyclic.cloud/api/'
+    baseURL: 'https://elated-ant-leotard.cyclic.app/api/'
 })
