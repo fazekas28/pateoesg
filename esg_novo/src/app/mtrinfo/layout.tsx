69d2a448c1b55bfde1bdb15874dc794d469c4f76
fazekas28 <148578127+fazekas28@users.noreply.github.com>
2023-10-31 14:28:31 -0300

    Update layout.tsx

--- esg_novo/src/app/mtrinfo/layout.tsx
+++ esg_novo/src/app/mtrinfo/layout.tsx
@@ -6,7 +6,7 @@ function MtrInfoPage({ children }: { children: React.ReactNode }) {
   <main className="bg-[url('https://res.cloudinary.com/diz14smvb/image/upload/v1697222289/eco-friendly-city-illustration_35669-144_mjnb8f.jpg')] bg-no-repeat bg-cover ">
    <Header />
    <div className="flex min-h-screen flex-col items-center py-5">
-    <form className='flex flex-col bg-lime-200 shadow-2xl px-6 py-5 rounded-2xl gap-10 text-gray-600 w-1/4 '>
+    <form className='flex flex-col bg-lime-200 shadow-2xl px-6 py-5 rounded-2xl gap-10 text-gray-600'>
      {children}
     </form>
    </div>
